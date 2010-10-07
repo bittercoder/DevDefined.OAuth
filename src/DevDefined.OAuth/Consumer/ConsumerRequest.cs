43114a0fd687f36704d4706187e8b1e555a70b3e
javicrespo <javiercrespoalvez@gmail.com>
2010-10-07 01:29:25 -0700

    Implementing oauth body hash client-side:Raw properties from RequestDescription to the actual request

--- src/DevDefined.OAuth/Consumer/ConsumerRequest.cs
+++ src/DevDefined.OAuth/Consumer/ConsumerRequest.cs
@@ -102,7 +102,7 @@ namespace DevDefined.OAuth.Consumer
           request.Proxy = new WebProxy(ProxyServerUri, false);
       }
 
-			if (description.Body != null)
+      if (!string.IsNullOrEmpty(description.Body))
       {
         request.ContentType = description.ContentType;
 
@@ -111,11 +111,13 @@ namespace DevDefined.OAuth.Consumer
           writer.Write(description.Body);
         }
       }
-      else if (!string.IsNullOrEmpty(description.Body))
+      else if (description.RawBody != null && description.RawBody.Length > 0)
       {
-          using (var writer = new StreamWriter(request.GetRequestStream()))
+          request.ContentType = description.ContentType;
+
+          using (var writer = new BinaryWriter(request.GetRequestStream()))
           {
-              writer.Write(description.Body);
+              writer.Write(description.RawBody);
           }
       }
 
