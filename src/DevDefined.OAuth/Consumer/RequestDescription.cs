18bf3cec422288d182ea5743a6e581c2e13c5688
javicrespo <javiercrespoalvez@gmail.com>
2010-10-07 01:23:30 -0700

    Implementing oauth body hash client-side: Adding RawBody to RequestDescription

--- src/DevDefined.OAuth/Consumer/RequestDescription.cs
+++ src/DevDefined.OAuth/Consumer/RequestDescription.cs
@@ -14,6 +14,7 @@ namespace DevDefined.OAuth.Consumer
     public string Method { get; set; }
     public string ContentType { get; set; }
     public string Body { get; set; }
+    public byte[] RawBody { get; set; }
     public NameValueCollection Headers { get; private set; }
   }
 }
